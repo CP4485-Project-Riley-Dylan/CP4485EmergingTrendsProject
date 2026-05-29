0c028601316b40386a4037d9c33ce407afe1bd9a
DylanTracey32 <dylan.tracey324@gmail.com>
2026-05-29 21:17:46 -0230

    Added nav bar in layout.js with 3 working pages: "Home", "Dashboard" and "Add Data".

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,13 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <nav>
+          <a href="/" >Home</a>
+          <a href="/dashboard" >Dashboard</a>
+          <a href="/add_data" >Add Data</a>
+        </nav>
+      </body>{children}
     </html>
   );
 }
