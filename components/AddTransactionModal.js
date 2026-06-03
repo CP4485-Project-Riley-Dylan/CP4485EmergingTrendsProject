70b894e47d4f2ac519168912d19ac5bf53822930
DylanTracey32 <dylan.tracey324@gmail.com>
2026-06-03 19:55:09 -0230

    Working transactionType radio buttons.

--- components/AddTransactionModal.js
+++ components/AddTransactionModal.js
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 export default function AddTransactionModal() {
-    return (
-        <>
-          <h2>Add Transaction</h2>
-          <form>
-            <label>Type</label>
-            <button>Expense</button>
-            <button>Income</button>
-          </form>
-        </>
-    )
+  const [transactionType, setTransactionType] = useState("");
+  return (
+    <>
+      <h2>Add Transaction</h2>
+      <form>
+        <label>Type</label>
+        <label>
+          <input
+            type="radio"
+            name="transactionType"
+            value="expense"
+            checked={transactionType === "expense"}
+            onChange={(e) => setTransactionType(e.target.value)}
+          />
+          Expense
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="transactionType"
+            value="income"
+            checked={transactionType === "income"}
+            onChange={(e) => setTransactionType(e.target.value)}
+          />
+          Income
+        </label>
+      </form>
+    </>
+  )
 }
