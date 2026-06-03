65213e8d970fd7eef291b55aa202b62c55d146e6
DylanTracey32 <dylan.tracey324@gmail.com>
2026-06-03 10:27:10 -0230

    End of work period. Left off creating initial AddTransactionModal.js react component

---
+++ components/AddTransactionModal.js
@@ -0,0 +1,12 @@
+export default function AddTransactionModal() {
+    return (
+        <>
+          <h2>Add Transaction</h2>
+          <form>
+            <label>Type</label>
+            <button>Expense</button>
+            <button>Income</button>
+          </form>
+        </>
+    )
+}
