fb16c07cd09750ce2b4b286efe5640c480d58f57
DylanTracey32 <dylan.tracey324@gmail.com>
2026-06-03 09:58:05 -0230

    Created income, expenses and net values which transactions will be affecting.

--- app/transactions/page.js
+++ app/transactions/page.js
@@ -1,4 +1,12 @@
+"use client";
+import { useState } from "react";
+
 export default function Page() {
+  const [income, setIncome] = useState("$0.00");
+  const [expenses, setExpenses] = useState("$0.00");
+  const [net, setNet] = useState("$0.00");
+
+
   return (
     <>
       <main>
@@ -6,6 +14,14 @@ export default function Page() {
           <h1>Transactions</h1>
           <p>All your financial activity in one place.</p>
           <button> + Add Transaction</button>
+        </section>
+        <section>
+          <p>Income</p>
+          <p>{income}</p>
+          <p>Expenses</p>
+          <p>{expenses}</p>
+          <p>Net</p>
+          <p>{net}</p>
         </section>
       </main>
     </>
