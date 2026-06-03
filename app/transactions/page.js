41ed73fe5f0e5aeef1c7592c2fe6bf2b116f0c7c
DylanTracey32 <dylan.tracey324@gmail.com>
2026-06-03 10:12:53 -0230

    Preparing for "Add Transaction" modal logic.

--- app/transactions/page.js
+++ app/transactions/page.js
@@ -2,9 +2,14 @@
 import { useState } from "react";
 
 export default function Page() {
+
+  //Data affected by transations
   const [income, setIncome] = useState("$0.00");
   const [expenses, setExpenses] = useState("$0.00");
   const [net, setNet] = useState("$0.00");
+
+  //Tab logic
+  const [isOpen, setIsOpen] = useState(false);
 
 
   return (
@@ -13,7 +18,8 @@ export default function Page() {
         <section className="welcome-section">
           <h1>Transactions</h1>
           <p>All your financial activity in one place.</p>
-          <button> + Add Transaction</button>
+          <button onClick={() => setIsOpen(true)}> + Add Transaction</button>
+          {/* {isOpen && <addTransaction></addTransaction>} */}
         </section>
         <section>
           <p>Income</p>
