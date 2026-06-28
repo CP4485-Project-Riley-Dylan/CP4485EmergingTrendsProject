0735a808aed437016a4b152b350cdca36c794c42
Riley <riley.druken26@ed.cna.nl.ca>
2026-06-28 12:15:35 -0230

    WIP KAN-62 KAN-63 DELETE and PUT routes in progress

---
+++ app/api/transactions/[id]/route.js
@@ -0,0 +1,26 @@
+import {NextReponse} from "next/server";
+import clientPromise from "@/lib/mongodb";
+import {ObjectId} from "mongodb";
+
+export async function PUT(request, { params }) {
+    try {
+        let body = await request.json();
+        let client = await clientPromise;
+        let db = client.db("budgetApp");
+        await db.collection("transactions").updateOne(
+            { _id: new ObjectId(params.id) },
+            { $set: body}
+        )
+        return NextResponse.json({message:"Updated"})
+    } catch (e) {
+        return NextResponse.json({ error: e.message }, { status: 500 });
+    }
+}
+
+export async function DELETE(request, { params }) {
+    try {
+
+    } catch (e) {
+        
+    }
+}
