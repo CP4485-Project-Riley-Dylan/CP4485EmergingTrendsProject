c4fa048804c839d29ca9673bc1d918fb7f104db4
DylanTracey32 <dylan.tracey324@gmail.com>
2026-08-09 19:20:02 -0230

    Implemented a test Groq AI action which always returns "Hello user!" to http://localhost:3000/api/aiRoute.

--- app/api/aiRoute/route.js
+++ app/api/aiRoute/route.js
@@ -1,52 +1,53 @@
-import {connectToDB} from '@/app/api/db';
-import { ObjectId } from 'mongodb';
+// import { connectToDB } from '@/app/api/db';
+// import { ObjectId } from 'mongodb';
 import { jwtVerify } from 'jose';
 import { cookies } from 'next/headers'
-import { recommendMovies } from '@/app/lib/ai/recommend-movies';
+import { doThing } from '@/lib/ai/aiActionInstance';
+// import { recommendMovies } from '@/app/lib/ai/recommend-movies';
 
 
-export async function POST(request) {
+export async function GET() /*Switched to GET request for testing until a user uploads data to it otherwise 405 error*/ {
     const cookieStore = await cookies()
-    const session = cookieStore.get('session')
+    const session = cookieStore.get('token')
     console.log(process.env.JWT_SECRET);
     console.log(session)
     const secret = new TextEncoder().encode(
-            process.env.JWT_SECRET)
+        process.env.JWT_SECRET)
     let payload = null;
     try {
-        ({payload} = await jwtVerify(session.value, secret))
+        ({ payload } = await jwtVerify(session.value, secret))
     }
     catch {
         console.log("issue with jwt api, redirecting to login")
-        return Response.json({error: 'Unauthorized'}, {status: 401})
+        return Response.json({ error: 'Unauthorized' }, { status: 401 })
     }
 
-    const {db} = await connectToDB();
+    // const { db } = await connectToDB();
 
-    const userId = payload.userId;
+    // const userId = payload.userId;
 
-    const movieList = await db.collection('movies').find( {userId: new ObjectId(userId)}).toArray();
-    //console.log(movieList);
+    // const movieList = await db.collection('movies').find( {userId: new ObjectId(userId)}).toArray();
+    // //console.log(movieList);
 
-    const cleanedMovies = movieList.map( (movie) => ({"title": movie.title, "rating": movie.rating}));
+    // const cleanedMovies = movieList.map( (movie) => ({"title": movie.title, "rating": movie.rating}));
 
-    //console.log(cleanedMovies);
+    // //console.log(cleanedMovies);
 
-    //return Response.json({"status": "testing"})
-    let recommendationResult;
+    // //return Response.json({"status": "testing"})
+    let response;
     try {
-        recommendationResult = await recommendMovies(cleanedMovies);
+        response = await doThing();
 
     }
-    catch(error) {
+    catch (error) {
         console.log(error)
-        return Response.json( {
+        return Response.json({
             error: "Recommendations could not be generated.  Please try again",
-            },
+        },
             {
                 status: 502
             })
     }
-    console.log(recommendationResult);
-    return Response.json(recommendationResult);
+    console.log(response);
+    return Response.json(response);
 }
